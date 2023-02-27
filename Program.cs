479ca33692c24da525faf0aa9c9705c3e3ef022b
MicroFish91 <mwfisher91@gmail.com>
2023-02-27 11:37:28 -0800

    base proj

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Worker;
+
+namespace testbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionsDebugger.Enable();
+
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
